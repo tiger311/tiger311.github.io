6fd8d451d7897a78bfec5fd5a7449290ecce75c3
tiger311 <alongyourway@qq.com>
2019-04-27 10:05:19 +0800

    jjjj

--- js/index.js
+++ js/index.js
@@ -55,7 +55,8 @@ let app = {
           divs[i].addEventListener('touchstart', app.commanderTouchStart);
           divs[i].addEventListener('touchmove', app.commanderTouchMove);
           divs[i].addEventListener('touchend', app.commanderTouchEnd);
-          divs[i].children[0].style.border = '2px solid yellow';
+          //divs[i].children[0].style.border = '2px solid yellow';
+          divs[i].style.border = '2px solid yellow';
         }
     },
     /////////// page1Loaded() 作用是在page1背景图load完毕之后才启动文本滑动进入
